b03394354fff88de937ddfdc3ae55a9b45bbaca7
heyji-ten <59608643+heyji-ten@users.noreply.github.com>
2022-02-07 17:53:09 +0900

    js작성중

---
+++ slider.js
@@ -0,0 +1,8 @@
+const slider = document.querySelector('#slider');
+const sliderList = document.querySelector('.slider-list');
+const sliderContent = document.querySelectorAll('.slider-content');
+
+const sliderSize = sliderContent.length;
+
+const prevBtn = document.querySelector('.prevBtn');
+const nextBtn = document.querySelector('.nextBtn');
