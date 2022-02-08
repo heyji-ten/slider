f71a937f5990908deffee96620550594455a4653
heyji-ten <59608643+heyji-ten@users.noreply.github.com>
2022-02-08 15:08:18 +0900

    기본 슬라이드 완성

--- slider.js
+++ slider.js
@@ -1,8 +1,32 @@
-const slider = document.querySelector('#slider');
-const sliderList = document.querySelector('.slider-list');
-const sliderContent = document.querySelectorAll('.slider-content');
+var slider= document.querySelector('.slider-list');
+var slide = document.querySelectorAll('.slider-content');
+var currentIdx = 0;
+var slideCount = slide.length;
+var prevBtn = document.querySelector('.prevBtn');
+var nextBtn = document.querySelector('.nextBtn');
+var slideWidth = 1000;
 
-const sliderSize = sliderContent.length;
+// slide 갯수에 따라 ul width 값 구하기
+slider.style.width = slideWidth * slideCount + 'px';
 
-const prevBtn = document.querySelector('.prevBtn');
-const nextBtn = document.querySelector('.nextBtn');
+// 슬라이드 이동
+function moveSlide(num) {
+    slider.style.left = -num * slideWidth + 'px';
+    currentIdx = num;
+}
+
+nextBtn.addEventListener('click', ()=>{
+    if(currentIdx < slideCount - 1){
+        moveSlide(currentIdx + 1);
+    } else {
+        moveSlide(0);
+    }
+});
+
+prevBtn.addEventListener('click', ()=>{
+    if(currentIdx > 0){
+        moveSlide(currentIdx - 1);
+    } else {
+        moveSlide(slideCount - 1);
+    }
+});
